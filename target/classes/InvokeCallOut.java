c562c92fbb221e51f8e2b5795c53d5222d98d987
Brigadesh <grebrigadesh@gmail.com>
2017-04-06 12:38:58 -0400

    Update InvokeCallOut.java

--- target/classes/InvokeCallOut.java
+++ target/classes/InvokeCallOut.java
@@ -1,4 +1,5 @@
-
+package CustomActivity;
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,53 +14,25 @@ import javax.tools.JavaFileObject;
 
 public class InvokeCallOut {
 	
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		String ToNumber="13213187287";
+		String BodySMS="Got+IT!";
 
-		  try {
-			
-			
-			  
-			URL url = new URL("https://app.hwlivetext.com/api/4.3/account/16788108775/message");
-			//api/4.3/account/16788108775/message
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-			
-			
-			//"application/json"
-			//application/x-www-form-urlencoded
-			//Account ID: 487c5776-e291-4152-8596-87b07b1b5c8a 
-			//Activation Code: 690d7269-0a2e-45a7-88a1-da264da42f8e
-			
-				
-			conn.setRequestProperty("AdminAccountId","487c5776-e291-4152-8596-87b07b1b5c8a");
-			conn.setRequestProperty("AdminPassword","690d7269-0a2e-45a7-88a1-da264da42f8e");
-			
-			String input = "{\"To\":\"16788108775\",\"Body\":\"Check Sending SMS\"}";
-			//String input = "\"Data\":{\"To\":+13213187287,\"Body\":\"Check Sending SMS\"}}";
-			byte[] outputInBytes = input.getBytes("UTF-8");
+  try {
+	
+	
+	  
+	URL url = new URL("https://app.hwlivetext.com/api/4.3/account/17206864418/message?to=%2B"+ToNumber+"&body="+BodySMS);
 
-			OutputStream os = conn.getOutputStream();
-			os.write(outputInBytes);
-			os.flush();
-			
-			System.out.println("Response Code:"+conn.getResponseCode()+conn.getResponseMessage()+" \n");
-		/*	if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-				throw new RuntimeException("Failed : HTTP error code : "
-					+ conn.getResponseCode() + conn.getResponseMessage());
-			}*/
-
-			/*BufferedReader br = new BufferedReader(new InputStreamReader(
-					(conn.getInputStream())));
-
-			String output;
-			System.out.println("Output from Server .... \n");
-			while ((output = br.readLine()) != null) {
-				System.out.println(output);
-			}*/
-
-			conn.disconnect();
+	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	conn.setDoOutput(true);
+	conn.setRequestMethod("POST");
+	conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");	
+	conn.setRequestProperty("AdminAccountId","487c5776-e291-4152-8596-87b07b1b5c8a");
+	conn.setRequestProperty("AdminPassword","690d7269-0a2e-45a7-88a1-da264da42f8e");
+	
+	System.out.println("Response Code:"+conn.getResponseCode()+conn.getInputStream()+" \n");
+	conn.disconnect();
 
 		  } catch (MalformedURLException e) {
 
